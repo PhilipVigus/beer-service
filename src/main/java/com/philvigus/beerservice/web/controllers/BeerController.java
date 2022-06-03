ab41bd7d700e7a0a0328caa9863c27483aca5d51
PhilipVigus <philipvigus@yahoo.co.uk>
2022-06-03 14:15:05 +0100

    Add BeerController

---
+++ src/main/java/com/philvigus/beerservice/web/controllers/BeerController.java
@@ -0,0 +1,31 @@
+package com.philvigus.beerservice.web.controllers;
+
+import com.philvigus.beerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+  @GetMapping("/{beerId}")
+  public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
+    // TODO: implement
+    return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+  }
+
+  @PostMapping
+  public ResponseEntity<Void> saveNewBeer(@RequestBody BeerDto beerDto) {
+    // TODO: implement
+    return new ResponseEntity<>(HttpStatus.CREATED);
+  }
+
+  @PutMapping("/{beerId}")
+  public ResponseEntity<Void> updateBeerById(
+      @PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+    // TODO: implement
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+}
