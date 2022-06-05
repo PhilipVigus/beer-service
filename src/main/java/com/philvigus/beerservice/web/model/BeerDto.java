1b6432fcd98c92d9367102973811de2f3b247c6f
PhilipVigus <philipvigus@yahoo.co.uk>
2022-06-05 15:53:11 +0100

    Add bean validation

--- src/main/java/com/philvigus/beerservice/web/model/BeerDto.java
+++ src/main/java/com/philvigus/beerservice/web/model/BeerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -14,15 +18,21 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDto {
-  private UUID id;
-  private Integer version;
+  @Null private UUID id;
 
-  private String name;
-  private BeerStyle style;
-  private Long upc;
-  private BigDecimal price;
+  @Null private Integer version;
+
+  @NotBlank private String name;
+
+  @NotNull private BeerStyle style;
+
+  @Positive @NotNull private Long upc;
+
+  @Positive @NotNull private BigDecimal price;
+
   private Integer quantity;
 
-  private OffsetDateTime createdAt;
-  private OffsetDateTime updatedAt;
+  @Null private OffsetDateTime createdAt;
+
+  @Null private OffsetDateTime updatedAt;
 }
