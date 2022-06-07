a06616bd54e56bd7f004086a7f24f4e9680ebe0b
PhilipVigus <philipvigus@yahoo.co.uk>
2022-06-07 17:36:24 +0100

    Add some basic doc type things

--- src/test/java/com/philvigus/beerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/philvigus/beerservice/web/controller/BeerControllerTest.java
@@ -4,20 +4,27 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.philvigus.beerservice.web.model.BeerDto;
 import com.philvigus.beerservice.web.model.BeerStyle;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BeerController.class)
+@AutoConfigureRestDocs
+@ExtendWith(RestDocumentationExtension.class)
 class BeerControllerTest {
   @Autowired MockMvc mockMvc;
 
@@ -27,9 +34,27 @@ class BeerControllerTest {
   void getBeerById() throws Exception {
     mockMvc
         .perform(
-            MockMvcRequestBuilders.get("/api/v1/beer/" + UUID.randomUUID().toString())
+            get("/api/v1/beer/{beerId}", UUID.randomUUID().toString())
+                .param("iscold", "yes")
                 .accept(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk());
+        .andExpect(status().isOk())
+        .andDo(
+            document(
+                "v1/beer",
+                pathParameters(
+                    parameterWithName("beerId").description("UUID of desired beer to get.")),
+                requestParameters(
+                    parameterWithName("iscold").description("Is beer cold query param")),
+                responseFields(
+                    fieldWithPath("id").description("Id of Beer"),
+                    fieldWithPath("version").description("Version number"),
+                    fieldWithPath("name").description("Beer name"),
+                    fieldWithPath("style").description("Beer style"),
+                    fieldWithPath("upc").description("Beer UPC"),
+                    fieldWithPath("price").description("Beer price"),
+                    fieldWithPath("quantity").description("Number of beers"),
+                    fieldWithPath("createdAt").description("Date created"),
+                    fieldWithPath("updatedAt").description("Date updated"))));
   }
 
   @Test
